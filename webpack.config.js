0d23dcc3fa76e3bceceef29121c82aea1d4dd6e2
Eric <ericxgao@gmail.com>
2021-03-27 14:11:43 -0700

    package fixes

--- webpack.config.js
+++ webpack.config.js
@@ -130,4 +130,8 @@ module.exports = {
     // new CompressionPlugin(),
     new CaseSensitivePathsPlugin(),
   ],
+  optimization: {
+    minimize: false, // <---- disables uglify.
+    // minimizer: [new UglifyJsPlugin()] if you want to customize it.
+  },
 };
