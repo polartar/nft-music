77e67603dc0585e86014adec14e4439e2e32df0c
Eric <ericxgao@gmail.com>
2021-04-21 16:44:46 -0700

    Fix cors

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const robots = require("express-robots-txt");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("*.js", function(req, res, next) {
   req.url = req.url + ".gz";
